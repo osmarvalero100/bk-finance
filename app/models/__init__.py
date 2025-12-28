c816510f39dedea4dd784c8d2b63527369080733
Osmar Valero <osmarvalero100@gmail.com>
2025-12-27 21:40:47 -0500

    Add Alembic

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,8 @@ from .investment import Investment
 from .financial_product import FinancialProduct
 from .debt import Debt
 from .budget import Budget, BudgetItem
+from .category import Category
+from .tag import Tag
+from .payment_method import PaymentMethod
 
-__all__ = ["User", "Expense", "Income", "Investment", "FinancialProduct", "Debt", "Budget", "BudgetItem"]
+__all__ = ["User", "Expense", "Income", "Investment", "FinancialProduct", "Debt", "Budget", "BudgetItem", "Category", "Tag", "PaymentMethod"]
